47b1f0bb84ce763b223eaff05f602389d6eeae22
luisfmachado <luismachado.pg@gmail.com>
2024-12-05 02:42:21 -0300

    feat: Relatório de Pagamentos

--- src/app/shared/dialog/dialog-filtros/dialog-filtros.component.ts
+++ src/app/shared/dialog/dialog-filtros/dialog-filtros.component.ts
@@ -1,11 +1,18 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { FornecedorService } from 'src/app/services/cadastro/fornecedor.service';
 import { FuncionarioService } from 'src/app/services/cadastro/funcionario.service';
 
 interface FuncionariosNome {
   id: number;
   value: string;
+}
+
+interface OpcaoF {
+  value: string;
+  id: number;
+  disabled: boolean;
 }
 
 @Component({
@@ -20,13 +27,16 @@ export class DialogFiltrosComponent implements OnInit {
   range: FormGroup;
   date: Date | null = null;
   funcionario: string = '';
+  fornecedor: string | null = null;
   listaFuncionario: FuncionariosNome[] = [];
+  opcoesF: OpcaoF[] = [];
 
   constructor(
     public dialogRef: MatDialogRef<DialogFiltrosComponent>,
     @Inject(MAT_DIALOG_DATA) public data: any,
     private fb: FormBuilder,
-    private _funcionarioService: FuncionarioService
+    private _funcionarioService: FuncionarioService,
+    private _fornecedorService: FornecedorService
   ) {
     this.range = this.fb.group({
       start: [null],
@@ -36,13 +46,15 @@ export class DialogFiltrosComponent implements OnInit {
 
   ngOnInit(): void {
     this.getNome();
+    this.carregaFornecedor();
   }
 
   dadosInput(): void {
     this.dialogRef.close({
       date: this.date,
       range: this.range.value,
-      funcionario: this.funcionario
+      funcionario: this.funcionario,
+      fornecedor: this.fornecedor
     });
   }
 
@@ -54,6 +66,19 @@ export class DialogFiltrosComponent implements OnInit {
           value: nome,
         }));
       }
+    });
+  }
+
+  private carregaFornecedor(): void {
+    this._fornecedorService.getFornecedor().subscribe({
+      next: (nomes) => {
+        this.opcoesF = nomes.map((fornecedor) => ({
+          id: fornecedor.id_fornece,
+          value: fornecedor.no_fornece,
+          disabled: false,
+        }));
+      },
+      error: (err) => console.error('Erro ao carregar fornecedores:', err),
     });
   }
 }
